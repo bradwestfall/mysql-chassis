81cc29395663272aca5a29622404b79e114014fd
Brad Westfall <brad@azpixels.com>
2016-10-26 09:11:45 -0700

    removing rollup

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-import babel from 'rollup-plugin-babel'
-
-export default {
-  entry: 'index.js',
-  dest: 'dist/mysql-chassis.js',
-  plugins: [ babel() ],
-  sourceMap: 'inline',
-  format: 'cjs'
-}
